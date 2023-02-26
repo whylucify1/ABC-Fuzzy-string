71a6ca741e6c01a5b25daec0df316601b36adeeb
Hermann J Fan <123210864+Junhao20@users.noreply.github.com>
2023-02-26 07:57:18 -0500

    Add files via upload

---
+++ test case generator/test_case_generator_UID_325.py
@@ -0,0 +1,77 @@
+# *********************
+
+# ------------------------------------
+# TEST CASE TYPE REFERENCE INFORMATION
+# ------------------------------------
+
+uid = 'UID-325' 
+theme = 'Special Characters and Spaces'
+category = 'Addition'
+sub_category = '1 special character added'
+entity_type = 'Individuals'
+
+# ********************
+
+#%%
+import numpy as np
+x = 5 # change when you want different results
+np.random.seed(x)
+# ------------------------------------
+# IMPORT DATA, PACKAGES, AND FUNCTIONS
+# ------------------------------------
+
+# import the relevant functions and packages
+
+import pandas as pd
+
+# download the OFAC list from the web
+
+ofac_list_download = pd.read_csv('https://www.treasury.gov/ofac/downloads/sdn.csv', header=None)
+ofac_list = ofac_list_download[[0,1,2]]
+ofac_list.columns = ['uid', 'name', 'entity_type']
+
+#%%
+
+# ------------------------------------------------------
+# FILTER FOR THE REQUIREMENTS OF THE NAME VARIATION TYPE
+# ------------------------------------------------------
+
+# filter for the requirements of the specific test case type
+
+ofac_list_filtered = ofac_list[(ofac_list.entity_type == 'individual')] # only evaluate entities
+# randomly choose 10 rows
+ofac_list_sampled = ofac_list_filtered.sample(n = 10)
+print(ofac_list_sampled)
+
+#%%
+
+import random
+
+def add_special_char(name):
+    # Choose a random index to insert the special character
+    special_char_index = random.randint(0, len(name))
+    # Choose a random special character
+    special_char = random.choice(['#', '@', '!', 
+                                  '$', '%', '^', 
+                                  '&', '*'])
+    # Insert the special character into the name string
+    modified_name = name[:special_char_index] + special_char + name[special_char_index:]
+    return modified_name
+
+#%%
+
+# ---------------------
+# CREATE THE TEST CASES
+# ---------------------
+
+# create blank final test cases table
+
+final_test_cases = pd.DataFrame(columns=['UID', 'Theme','Category','Sub-category','Entity-Type','Test Case ID' , 'OFAC List UID', 'Original Name','Test Case Name'])
+#run loop to generate the test cases 
+import random
+
+for index, row in ofac_list_sampled.iterrows():
+   active_name = row['name']
+   final_name = add_special_char(active_name)
+   final_test_cases.loc[len(final_test_cases)] = [uid, theme, category, sub_category, entity_type, uid + ' - ' + str(index), row['uid'], row['name'], final_name.upper()] # append to the dataframe
+print(final_test_cases)
