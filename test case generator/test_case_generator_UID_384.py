ffcd671886b175cd9faab1d69376e849cb684de0
Hermann J Fan <123210864+Junhao20@users.noreply.github.com>
2023-02-27 00:02:16 -0500

    Add files via upload

---
+++ test case generator/test_case_generator_UID_384.py
@@ -0,0 +1,74 @@
+# ********************
+
+# ------------------------------------
+# TEST CASE TYPE REFERENCE INFORMATION
+# ------------------------------------
+
+uid = 'UID-384' 
+theme = 'Special characters and spaces'
+category = 'Split'
+sub_category = '1 name part split'
+entity_type = 'Entity'
+
+# ********************
+
+#%%
+import numpy as np
+x = 5 # change when you want different results
+np.random.seed(x)
+# ------------------------------------
+# IMPORT DATA, PACKAGES, AND FUNCTIONS
+# ------------------------------------
+
+# import the relevant functions and packages
+
+import pandas as pd
+from test_case_generator_functions import separate_words_in_name_3
+# download the OFAC list from the web
+
+ofac_list_download = pd.read_csv('https://www.treasury.gov/ofac/downloads/sdn.csv', header=None)
+ofac_list = ofac_list_download[[0,1,2]]
+ofac_list.columns = ['uid', 'name', 'entity_type']
+
+#%%
+
+# ------------------------------------------------------
+# FILTER FOR THE REQUIREMENTS OF THE NAME VARIATION TYPE
+# ------------------------------------------------------
+
+# filter for the requirements of the specific test case type
+
+ofac_list_filtered = ofac_list[(ofac_list.entity_type == '-0- ')] # only evaluate entities
+# randomly choose 10 rows
+ofac_list_sampled = ofac_list_filtered.sample(n = 10)
+print(ofac_list_sampled)
+
+
+#%%
+
+def split_name_part_with_space(name_part, split_index):
+    # Split the name part into two parts at the given index
+    first_part = name_part[:split_index]
+    second_part = name_part[split_index:]
+    # Concatenate the two parts with a space in between
+    modified_name_part = first_part + ' ' + second_part
+    return modified_name_part
+
+#%%
+
+# ---------------------
+# CREATE THE TEST CASES
+# ---------------------
+
+# create blank final test cases table
+
+final_test_cases = pd.DataFrame(columns=['UID', 'Theme','Category','Sub-category','Entity-Type','Test Case ID' , 'OFAC List UID', 'Original Name','Test Case Name'])
+#run loop to generate the test cases 
+import random
+
+for index, row in ofac_list_sampled.iterrows():
+   active_name = row['name']
+   final_name = split_name_part_with_space(active_name, 7)
+   final_test_cases.loc[len(final_test_cases)] = [uid, theme, category, sub_category, entity_type, uid + ' - ' + str(index), row['uid'], row['name'], final_name.upper()] # append to the dataframe
+print(final_test_cases)
+
