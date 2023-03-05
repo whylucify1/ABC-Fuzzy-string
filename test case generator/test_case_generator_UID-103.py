60af5067f4100ad418db5aea3faf3ea419d3864d
ccraffen <82552106+ccraffen@users.noreply.github.com>
2023-03-04 20:13:30 -0500

    Add files via upload

---
+++ test case generator/test_case_generator_UID-103.py
@@ -0,0 +1,65 @@
+# ********************
+
+# ------------------------------------
+# TEST CASE TYPE REFERENCE INFORMATION
+# ------------------------------------
+
+uid = 'UID-103' 
+theme = 'Modified Order'
+category = 'Name Part Swap'
+sub_category = '2 Name Parts Swapped at the end'
+entity_type = 'entity'
+
+# ********************
+
+#%%
+import numpy as np
+x = 5 # change when you want different results
+np.random.seed(x)
+# ------------------------------------
+# IMPORT DATA, PACKAGES, AND FUNCTIONS
+# ------------------------------------
+
+# import the relevant functions and packages
+
+import pandas as pd
+from test_case_generator_functions import rand_bus_designator
+# download the OFAC list from the web
+
+ofac_list_download = pd.read_csv('https://www.treasury.gov/ofac/downloads/sdn.csv', header=None)
+ofac_list = ofac_list_download[[0,1,2]]
+ofac_list.columns = ['uid', 'name', 'entity_type']
+
+#%%
+
+# ------------------------------------------------------
+# FILTER FOR THE REQUIREMENTS OF THE NAME VARIATION TYPE
+# ------------------------------------------------------
+
+# filter for the requirements of the specific test case type
+
+ofac_list_filtered = ofac_list[(ofac_list.entity_type == '-0- ')] # only evaluate entities
+# randomly choose 10 rows
+ofac_list_sampled = ofac_list_filtered.sample(n = 10)
+print(ofac_list_sampled)
+
+#%%
+
+# ---------------------
+# CREATE THE TEST CASES
+# ---------------------
+
+# create blank final test cases table
+
+final_test_cases = pd.DataFrame(columns=['UID', 'Theme','Category','Sub-category','Entity-Type','Test Case ID' , 'OFAC List UID', 'Original Name','Test Case Name'])
+
+#run loop to generate the test cases 
+for index, row in ofac_list_sampled.iterrows():
+    busdes_name = row['name']
+    words = busdes_name.split()
+    last_two_words = words[-2:][::-1]
+    final_test_name = " ".join(words[:-2] + last_two_words)
+    final_test_name = final_test_name.title()
+    final_test_name = final_test_name.upper()
+    final_test_cases.loc[len(final_test_cases)] = [uid, theme, category, sub_category, entity_type, uid + ' - ' + str(index), row['uid'], row['name'], final_test_name.upper()]
+    print(final_test_name)
