5f56b258f2ab5e506cf890feb51ea9e9b18d2307
ccraffen <82552106+ccraffen@users.noreply.github.com>
2023-03-06 16:35:55 -0500

    Add files via upload

---
+++ test case generator/test_case_generator_UID-65.py
@@ -0,0 +1,67 @@
+# ********************
+
+# ------------------------------------
+# TEST CASE TYPE REFERENCE INFORMATION
+# ------------------------------------
+
+uid = 'UID-65' 
+theme = 'Name Deletions'
+category = 'First Name Part'
+sub_category = '2 name parts removed'
+entity_type = 'entity'
+
+# ********************
+def remove_two_names(text):
+    words = text.split()
+    if len(words) > 1:
+        return " ".join(words[2:])
+    else:
+        return text
+
+#%%
+import numpy as np
+x = 5 # change when you want different results
+np.random.seed(x)
+# ------------------------------------
+# IMPORT DATA, PACKAGES, AND FUNCTIONS
+# ------------------------------------
+
+# import the relevant functions and packages
+
+import pandas as pd
+from test_case_generator_functions import remove_two_names
+# download the OFAC list from the web
+
+ofac_list_download = pd.read_csv('https://www.treasury.gov/ofac/downloads/sdn.csv', header=None)
+ofac_list = ofac_list_download[[0,1,2]]
+ofac_list.columns = ['uid', 'name', 'entity_type']
+
+#%%
+
+# ------------------------------------------------------
+# FILTER FOR THE REQUIREMENTS OF THE NAME VARIATION TYPE
+# ------------------------------------------------------
+
+# filter for the requirements of the specific test case type
+
+ofac_list_filtered = ofac_list[(ofac_list.entity_type == '-0- ')] # only evaluate entities
+# randomly choose 10 rows
+ofac_list_sampled = ofac_list_filtered.sample(n = 10)
+print(ofac_list_sampled)
+
+#%%
+
+# ---------------------
+# CREATE THE TEST CASES
+# ---------------------
+
+# create blank final test cases table
+
+final_test_cases = pd.DataFrame(columns=['UID', 'Theme','Category','Sub-category','Entity-Type','Test Case ID' , 'OFAC List UID', 'Original Name','Test Case Name'])
+
+#run loop to generate the test cases 
+for index, row in ofac_list_sampled.iterrows():
+    busdes_names = row['name']
+    final_test_name = remove_two_names(busdes_names)
+    final_test_cases.loc[len(final_test_cases)] = [uid, theme, category, sub_category, entity_type, uid + ' - ' + str(index), row['uid'], row['name'], final_test_name.upper()]
+    print(final_test_name)
