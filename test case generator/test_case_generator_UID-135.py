2a2ed58bb23e1bab9139da4d8acf4a29f0ed224e
Amanda MK <111911543+cashewmonster@users.noreply.github.com>
2023-03-14 14:28:39 -0400

    Add files via upload

--- test case generator/test_case_generator_UID-135.py
+++ test case generator/test_case_generator_UID-135.py
@@ -1,7 +1,7 @@
-uid = 'UID-137' 
+uid = 'UID-135' 
 theme = 'Name Part Variations'
 category = 'initials'
-sub_category = '2 middle name initial'
+sub_category = '> 2 middle name initial'
 entity_type = 'individual'
 
 # ********************
@@ -14,6 +14,38 @@ np.random.seed(x)
 # IMPORT DATA, PACKAGES, AND FUNCTIONS
 # ------------------------------------
 
+def separate_words_in_name_5(name):
+    first_name = name[name.find(', ') + 2:name.find(' ', name.find(', ') + 2)]
+    last_name = name[:name.find(',')]
+    
+    middle_name = name[name.find(' ', name.find(', ') + 2) + 1:]
+    second_space = middle_name.find(' ')
+    third_space = middle_name.find(' ', second_space + 1)
+    fourth_space = middle_name.find(' ', third_space + 1)
+    
+    if third_space != -1 and fourth_space != -1:
+        # There are at least three middle names, so extract the third and fourth middle names
+        third_middle_name = middle_name[third_space + 1:fourth_space]
+        fourth_middle_name = middle_name[fourth_space + 1:]
+        # Extract the first, second, and third middle names
+        second_middle_name = middle_name[second_space + 1:third_space]
+        first_middle_name = middle_name[:second_space]
+    elif third_space != -1:
+        # There are exactly three middle names, so the fourth middle name is an empty string
+        third_middle_name = middle_name[third_space + 1:]
+        fourth_middle_name = ""
+        # Extract the first, second, and third middle names
+        second_middle_name = middle_name[second_space + 1:third_space]
+        first_middle_name = middle_name[:second_space]
+    else:
+        # There are not at least three middle names, so all middle names are empty strings
+        first_middle_name = ""
+        second_middle_name = ""
+        third_middle_name = ""
+        fourth_middle_name = ""
+    
+    separated_words = [first_name, first_middle_name, second_middle_name, third_middle_name, fourth_middle_name, last_name]
+    return separated_words
 
 # import the relevant functions and packages
 
@@ -61,6 +93,7 @@ for index, row in ofac_list_sampled.iterrows():
         separated_words = separate_words_in_name_5(row['name'])
         abbreviated_middle_name1 = separated_words[1][0] + '.'
         abbreviated_middle_name2 = separated_words[2][0] + '.'
-        final_test_name = separated_words[0] + " " + abbreviated_middle_name1 + " " + abbreviated_middle_name2 + " "  + separated_words[3] + " " + separated_words[4]+ " "+ separated_words[5]
+        abbreviated_middle_name3 = separated_words[3][0] + '.'
+        final_test_name = separated_words[0] + " " + abbreviated_middle_name1 + " " + abbreviated_middle_name2 + " " + abbreviated_middle_name3 + " " + separated_words[4]+" " + separated_words[5]
         final_test_cases.loc[len(final_test_cases)] = [uid, theme, category, sub_category, entity_type, uid + ' - ' + str(index), row['uid'], row['name'], final_test_name.upper()]
         print(final_test_name)
