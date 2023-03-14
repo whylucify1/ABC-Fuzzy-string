a2c190b95220c2073e2e150e647d4cc8616661c5
Hermann J Fan <123210864+Junhao20@users.noreply.github.com>
2023-03-14 14:33:54 -0400

    Add files via upload

---
+++ test case generator/test_case_generator_UID_88.py
@@ -0,0 +1,73 @@
+
+# ********************
+
+# ------------------------------------
+# TEST CASE TYPE REFERENCE INFORMATION
+# ------------------------------------
+
+uid = 'UID-88' 
+theme = 'Name deletions'
+category = 'qualifier'
+sub_category = '1 qualifier removed'
+entity_type = 'Entity'
+
+# ********************
+
+#%%
+
+# ------------------------------------
+# IMPORT DATA, PACKAGES, AND FUNCTIONS
+# ------------------------------------
+
+# import the relevant functions and packages
+
+import pandas as pd
+from test_case_generator_functions import separate_words_in_name_2, separate_words_in_name_3
+
+# download the OFAC list from the web
+
+ofac_list_download = pd.read_csv('https://www.treasury.gov/ofac/downloads/sdn.csv', header=None)
+ofac_list = ofac_list_download[[0,1,2]]
+ofac_list.columns = ['uid', 'name', 'entity_type']
+
+#%%
+
+# ------------------------------------------------------
+# FILTER FOR THE REQUIREMENTS OF THE NAME VARIATION TYPE
+# ------------------------------------------------------
+
+# filter for the requirements of the specific test case type
+
+ofac_list_filtered = ofac_list[(ofac_list.entity_type == 'individual')] # only evaluate individuals
+ofac_list_filtered = ofac_list_filtered[(ofac_list_filtered.name.str.count(' ') < 3)] # only evaluate names with two or three words
+
+# randomly choose 10 rows
+
+ofac_list_sampled = ofac_list_filtered.sample(n = 10)
+print(ofac_list_sampled)
+#%%
+
+# ---------------------
+# CREATE THE TEST CASES
+# ---------------------
+
+# create blank final test cases table
+
+final_test_cases = pd.DataFrame(columns=['UID', 'Theme','Category','Sub-category','Entity-Type','Test Case ID' , 'OFAC List UID', 'Original Name','Test Case Name'])
+
+# run loop to generate the test cases
+
+import random
+import re
+
+for index, row in ofac_list_sampled.iterrows():
+    name = row['name']
+    qualifiers = ["Jr", "Sr", "i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix", "x", "Prof"]
+    # Create a regular expression to match the qualifiers
+    qualifier_pattern = r"\b(" + "|".join(qualifiers) + r")\b"
+
+    # Use the regular expression to remove the qualifiers from the name
+    clean_name = re.sub(qualifier_pattern, "", name, flags=re.IGNORECASE)
+    final_name = clean_name
+    final_test_cases.loc[len(final_test_cases)] = [uid, theme, category, sub_category, entity_type, uid + '-' + str(index), row['uid'], row['name'], final_name]
+print(final_test_cases)
