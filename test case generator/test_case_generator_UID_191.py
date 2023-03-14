602a4c0ced1f9e1b491d3c64804475d3577a9811
Hermann J Fan <123210864+Junhao20@users.noreply.github.com>
2023-03-14 14:04:17 -0400

    Add files via upload

---
+++ test case generator/test_case_generator_UID_191.py
@@ -0,0 +1,68 @@
+
+# ********************
+
+# ------------------------------------
+# TEST CASE TYPE REFERENCE INFORMATION
+# ------------------------------------
+
+uid = 'UID-191' 
+theme = 'Names where names parts are modified'
+category = 'Character removal in the middle of the name part'
+sub_category = '1 character removal at the middle'
+entity_type = 'Entity'
+
+# ********************
+
+#%%
+
+# ------------------------------------
+# IMPORT DATA, PACKAGES, AND FUNCTIONS
+# ------------------------------------
+
+# import the relevant functions and packages
+
+import pandas as pd
+from test_case_generator_functions import separate_words_in_name_2, separate_words_in_name_3
+
+# download the OFAC list from the web
+
+ofac_list_download = pd.read_csv('https://www.treasury.gov/ofac/downloads/sdn.csv', header=None)
+ofac_list = ofac_list_download[[0,1,2]]
+ofac_list.columns = ['uid', 'name', 'entity_type']
+
+#%%
+
+# ------------------------------------------------------
+# FILTER FOR THE REQUIREMENTS OF THE NAME VARIATION TYPE
+# ------------------------------------------------------
+
+# filter for the requirements of the specific test case type
+
+ofac_list_filtered = ofac_list[(ofac_list.entity_type == '-0- ')] # only evaluate individuals
+ofac_list_filtered = ofac_list_filtered[(ofac_list_filtered.name.str.count(' ') < 3)] # only evaluate names with two or three words
+
+# randomly choose 10 rows
+
+ofac_list_sampled = ofac_list_filtered.sample(n = 10)
+print(ofac_list_sampled)
+#%%
+
+# ---------------------
+# CREATE THE TEST CASES
+# ---------------------
+
+# create blank final test cases table
+
+final_test_cases = pd.DataFrame(columns=['UID', 'Theme','Category','Sub-category','Entity-Type','Test Case ID' , 'OFAC List UID', 'Original Name','Test Case Name'])
+
+# run loop to generate the test cases
+
+for index, row in ofac_list_sampled.iterrows():
+    active_name = row['name']
+    name = active_name
+    middle_index = len(name)//2
+    final_name = name[:middle_index] + name[middle_index+1:]
+    final_test_cases.loc[len(final_test_cases)] = [uid, theme, category, sub_category, entity_type, uid + '-' + str(index), row['uid'], row['name'], final_name]
+print(final_test_cases)
+   
+    
